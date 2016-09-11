213a1d6d03693dd5232077ae2830a384f1f40aaa
kostisgerakos <kostisgerakos@gmail.com>
2016-09-11 20:35:25 +0300

    comment adapter

---
+++ app/src/main/java/com/di/raine/adapters/ReviewAdapter.java
@@ -0,0 +1,75 @@
+package com.di.raine.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.di.raine.R;
+import com.di.raine.branches.Comment;
+
+
+import java.util.List;
+
+/**
+ * Created by kostis on 11/9/2016.
+ */
+public class ReviewAdapter extends BaseAdapter {
+    private List<Comment> rReviewsList;
+    private LayoutInflater rInflater;
+
+
+    public ReviewAdapter(List<Comment> list, LayoutInflater inflater) {
+        rReviewsList = list;
+        rInflater = inflater;
+    }
+
+    @Override
+    public int getCount() {
+        return rReviewsList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return rReviewsList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewComment comment;
+
+        if (convertView == null) {
+            convertView = rInflater.inflate(R.layout.reviewrow,
+                    null);
+            comment = new ViewComment();
+
+            comment.commentText = (TextView) convertView.findViewById(R.id.TextViewReviewText);
+
+            comment.commentGrade = (TextView) convertView.findViewById(R.id.TextViewreviewGrade);
+
+            convertView.setTag(comment);
+        } else {
+            comment = (ViewComment) convertView.getTag();
+        }
+
+        Comment curComment = rReviewsList.get(position);
+        comment.commentText.setText(curComment.getTextMessage());
+        comment.commentGrade.setText(curComment.getRating());
+
+        return convertView;
+    }
+
+
+    private class ViewComment {
+
+        TextView commentText;
+        TextView commentGrade;
+    }
+}
